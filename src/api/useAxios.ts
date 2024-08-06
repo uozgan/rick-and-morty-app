3074db1336c4b107f78ba5e77968a4638c4e21d1
Utku Ozgan <utkuozgan@Utkus-MacBook-Air.local>
2024-08-06 17:31:21 +0200

    useAxios hook builded

---
+++ src/api/useAxios.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { baseApiUrl } from "../config/constants";
+axios.defaults.baseURL = baseApiUrl;
+
+interface UseAxiosProps {
+  url: string;
+}
+
+const useAxios = ({ url }: UseAxiosProps) => {
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError | string>("");
+  const [loading, setLoading] = useState<Boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(url);
+        setResponse(res);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          setError(err);
+        } else {
+          setError("An unexpected error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { response, error, loading };
+};
+
+export default useAxios;
---
+++ src/config/constants.ts
@@ -0,0 +1,1 @@
+export const baseApiUrl = "https://rickandmortyapi.com/api";
